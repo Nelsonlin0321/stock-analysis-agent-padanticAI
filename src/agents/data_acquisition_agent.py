8356b82b1558cb396983f75ed8312faf81732d58
Nelson Lin <nelsonlin0321@outlook.com>
2025-05-09 21:45:30 +0800

    Enhance data acquisition agent to fetch and structure NASDAQ performance data
    - Updated the system prompt to clarify the agent's task of fetching top NASDAQ performance stock data.
    - Integrated asynchronous functionality using AsyncClient for improved performance.
    - Added a main function to run the data acquisition agent and print the structured response.
    - Modified the get_top_nasdaq_performance_stock_data tool to save fetched data as a CSV file and return its path.

--- src/agents/data_acquisition_agent.py
+++ src/agents/data_acquisition_agent.py
@@ -1,13 +1,36 @@
+import asyncio
+from anthropic import AsyncClient
 from pydantic_ai import Agent
 from src.models import deepseek
+from src.schema import Deps
+from devtools import debug
+from src.tools import get_top_nasdaq_performance_stock_data
 
 data_acquisition_agent = Agent(
     model=deepseek,
     system_prompt=(
-        'Be concise, reply with one sentence. '
-        'Use the `get_stock_data` tool to fetch stock data'
-        'then identify the day\'s top NASDAQ gainer (symbol and percentage increase).'
+        'You are a data acquisition agent. '
+        'You task is to use the `get_top_nasdaq_performance_stock_data` tool to fetch top nasdaq performance stock_data for further analysis. '
+        'You should use the tool to fetch the data and return it in a structured format. '
+        'The data should include the stock symbol, percentage increase, sample data in markdown format, pandas dataframe info, and csv data path for python code analysis. '
     ),
     retries=3,
     instrument=True,
+    tools=[get_top_nasdaq_performance_stock_data]
 )
+
+
+async def main():
+    async with AsyncClient() as client:
+        deps = Deps(
+            client=client, number_of_days=5
+        )
+        result = await data_acquisition_agent.run(
+            'Start', deps=deps
+        )
+        debug(result)
+        print('Response:', result.output)
+
+if __name__ == '__main__':
+    asyncio.run(main())
+    # python -m src.agents.data_acquisition_agent
--- src/tools.py
+++ src/tools.py
@@ -1,3 +1,4 @@
+import os
 from pydantic_ai import RunContext
 from src.schema import Deps
 from src.utils_stock import capture_df_info, convert_to_markdown_table, fetch_stock_data, get_top_nasdaq_performance_stock
@@ -17,6 +18,8 @@ async def get_top_nasdaq_performance_stock_data(
         symbol=symbol, number_of_days=ctx.deps.number_of_days)
     sample_data_in_markdown = convert_to_markdown_table(df)
     data_path = f"{symbol}_performance_in_the_past_{ctx.deps.number_of_days}.csv".lower()
+    data_path = os.path.abspath(data_path)
+    df.to_csv(data_path, index=False)
     pandas_dataframe_info = capture_df_info(df)
 
     return {
